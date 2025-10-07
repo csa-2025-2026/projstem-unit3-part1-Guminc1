3b34b2022931d6b417b6152f08a8deb96be90a79
Guminc1 <zombiekillerranger@gmail.com>
2025-10-07 18:02:53 +0000

    Finish

--- Main.java
+++ Main.java
@@ -4,6 +4,13 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Rectangle rec1 = new Rectangle(3.2,5.8);
+    Rectangle rec2 = new Rectangle(5.8,3.2);
+    boolean isRotated = (rec1.getLength() == rec2.getWidth()) && (rec1.getWidth() == rec2.getLength());
+    System.out.println(isRotated);
+    boolean isCongruent = rec1.equals(rec2) || isRotated;
+    System.out.println(isCongruent);
+    boolean isSimular = isCongruent || ((rec1.getWidth() / rec2.getWidth()) == (rec1.getLength() / rec2.getLength()));
+  System.out.println(isSimular);
   }
 }
